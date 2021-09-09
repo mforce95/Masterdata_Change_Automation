f23258e8a769e21c7c02f704e0cb4050e5527639
mforce95 <marcih@windowslive.com>
2021-09-09 14:19:55 +0200

    Update MasterData_Automation.py

--- MasterData_Automation.py
+++ MasterData_Automation.py
@@ -1,23 +1,28 @@
 import pyautogui
+import playsound
 import clipboard
 import time
+import sys
 
-while True:
+
 ### EXCEL PART START ###
 
 ## Open Brave Browser
-    bravebrowser=pyautogui.locateCenterOnScreen('images/bravebrowser.png', confidence=0.9) 
-    if not bravebrowser:
-        print('Error locating Brave browser')
-        break
-
+bravebrowser=pyautogui.locateCenterOnScreen('images/bravebrowser.png', confidence=0.9) 
+if not bravebrowser:
+    print('Error locating Brave browser.')
+    playsound('/sound/sad.mp3')
+    sys.exit()
+else:
     pyautogui.click(bravebrowser)
     time.sleep(0.2)
 
+while True:
 ## Click on GTIN column
     excel_gtin=pyautogui.locateCenterOnScreen('images/excel_gtin.png', grayscale=True)
     if not excel_gtin:
-        print('Error locating GTIN column in Excel')
+        print('Error locating GTIN column in Excel. Last GTIN:',clipboard.paste())
+        playsound('/sound/sad.mp3')
         break
 
     pyautogui.click(excel_gtin)
@@ -26,16 +31,18 @@ while True:
 ## Click on Filters menu
     excel_filtermenu=pyautogui.locateCenterOnScreen('images/excel_filtermenu.png', grayscale=True)  
     if not excel_filtermenu:
-        print('Error locating the filter menu in Excel')
+        print('Error locating the filter menu in Excel. Last GTIN:',clipboard.paste())
+        playsound('/sound/sad.mp3')
         break
 
     pyautogui.click(excel_filtermenu)
     time.sleep(0.5)
 
 ## Click on ReApply Filters
-    excel_refilter=pyautogui.locateCenterOnScreen('images/excel_refilter.png', grayscale=True)
+    excel_refilter=pyautogui.locateCenterOnScreen('images/excel_refilter.png')
     if not excel_refilter:
-        print('Error locating the reapply button in the filter menu in Excel')
+        print('Error locating the reapply button in the filter menu in Excel. Last GTIN:',clipboard.paste())
+        playsound('/sound/sad.mp3')
         break
 
     pyautogui.click(excel_refilter)
@@ -57,7 +64,8 @@ while True:
 ## Open Remote Desktop
     remotedesktop=pyautogui.locateCenterOnScreen('images/remotedesktop.png', confidence=0.9) 
     if not remotedesktop:
-        print('Error locating Remote Desktop')
+        print('Error locating Remote Desktop. Last GTIN:',clipboard.paste())
+        playsound('/sound/sad.mp3')
         break
 
     pyautogui.click(remotedesktop)
@@ -66,7 +74,8 @@ while True:
 ## Remove existing GTIN entry
     mt_removegtin=pyautogui.locateCenterOnScreen('images/mt_removegtin.png') 
     if not mt_removegtin:
-        print('Error locating GTIN entry clearing button in MT')
+        print('Error locating GTIN entry clearing button in MT. Last GTIN:',clipboard.paste())
+        playsound('/sound/sad.mp3')
         break
 
     pyautogui.click(mt_removegtin)
@@ -86,7 +95,8 @@ while True:
 ## Click on Apply filters
     mt_apply=pyautogui.locateCenterOnScreen('images/mt_apply.png') 
     if not mt_apply:
-        print('Error locating Apply Filter button in MT')
+        print('Error locating Apply Filter button in MT. Last GTIN:',clipboard.paste())
+        playsound('/sound/sad.mp3')
         break
 
     pyautogui.click(mt_apply)
@@ -95,7 +105,8 @@ while True:
 ## Click on expanding the GTIN versions
     mt_openmenu=pyautogui.locateCenterOnScreen('images/mt_openmenu.png', grayscale=True) 
     if not mt_openmenu:
-        print('Error locating the expanding button of the GTIN in MT')
+        print('Error locating the expanding button of the GTIN in MT. Last GTIN:',clipboard.paste())
+        playsound('/sound/sad.mp3')
         break
 
     pyautogui.click(mt_openmenu)
@@ -116,7 +127,8 @@ while True:
                         if not version2:
                             version1=pyautogui.locateCenterOnScreen('images/version1.png')
                             if not version1:
-                                print('Error locating the GTIN version in MT')
+                                print('Error locating the GTIN version in MT. Last GTIN:',clipboard.paste())
+                                playsound('/sound/sad.mp3')
                                 break
                             else:
                                 pyautogui.click(version1, button='right')
@@ -138,7 +150,8 @@ while True:
 ## Select creating a new version of the MasterData
     mt_masterdatamenu=pyautogui.locateCenterOnScreen('images/mt_masterdatamenu.png') 
     if not mt_masterdatamenu:
-        print('Error locating the MasterData menu in MT')
+        print('Error locating the MasterData menu in MT. Last GTIN:',clipboard.paste())
+        playsound('/sound/sad.mp3')
         break
 
     pyautogui.click(mt_masterdatamenu)
@@ -146,7 +159,8 @@ while True:
 
     mt_newversion=pyautogui.locateCenterOnScreen('images/mt_newversion.png') 
     if not mt_newversion:
-        print('Error locating the MasterData new version button in MT')
+        print('Error locating the MasterData new version button in MT. Last GTIN:',clipboard.paste())
+        playsound('/sound/sad.mp3')
         break
 
     pyautogui.click(mt_newversion)
@@ -155,7 +169,8 @@ while True:
 ## Click on the MAH selector button
     mt_mah=pyautogui.locateCenterOnScreen('images/mt_mah.png') 
     if not mt_mah:
-        print('Error locating the MAH selector button in MT')
+        print('Error locating the MAH selector button in MT. Last GTIN:',clipboard.paste())
+        playsound('/sound/sad.mp3')
         break
 
     pyautogui.click(mt_mah)
@@ -165,13 +180,13 @@ while True:
 
 ## Wait for the EMVO Answer // MISSING
 
-## If successfull maximum 30 seconds  // MISSING
 
 
 ## Minimize the Remote Desktop window
     rd_minimize=pyautogui.locateCenterOnScreen('images/rd_minimize.png', confidence=0.9) 
     if not rd_minimize:
-        print('Error locating the button to minimize the Remote Desktop window')
+        print('Error locating the button to minimize the Remote Desktop window. Last GTIN:',clipboard.paste())
+        playsound('/sound/sad.mp3')
         break
 
     pyautogui.click(rd_minimize)
@@ -181,9 +196,11 @@ while True:
     excel_x=pyautogui.locateCenterOnScreen('images/excel_x.png', grayscale=True)
     
     if not excel_x:
-        print('Error locating Update column in Excel')
+        print('Error locating Update column in Excel. Last GTIN:',clipboard.paste())
+        playsound('/sound/sad.mp3')
         break
-
+        
+## If successfull maximum 30 seconds
     pyautogui.click(excel_x)
     time.sleep(0.1)
     pyautogui.press('down', interval=0.1)
